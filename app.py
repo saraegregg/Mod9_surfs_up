1a2ccb90d312d25af1a665f7ec94fabd249b7ba1
Sara <95581411+saraegregg@users.noreply.github.com>
2022-03-01 10:46:59 -0600

    Adding challenge code and images.

--- app.py
+++ app.py
@@ -32,45 +32,53 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return(
-    '''
-    Welcome to the Hawaii Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/temp/start/end
-    ''')
+    """
+    Welcome to the Hawaii Climate Analysis API!<br>
+    Available Routes:<br>
+    /api/v1.0/precipitation<br>
+    /api/v1.0/stations<br>
+    /api/v1.0/tobs<br>
+    /api/v1.0/temp/&lt;start&gt;/&lt;end&gt;<br>
+    """)
 
 #create the precip route and corresponding function
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    #Create session link from Python to database
+    session = Session(engine)
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     precipitation = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= prev_year).all()
     precip = {date: prcp for date, prcp in precipitation}
+    session.close()
     return jsonify(precip)
 
 #create the stations route and function
 @app.route("/api/v1.0/stations")
 def stations():
+    session = Session(engine)
     results = session.query(Station.station).all()
     stations = list(np.ravel(results))
+    session.close()
     return jsonify(stations=stations)
 
 #create temp obs route and function
 @app.route("/api/v1.0/tobs")
 def temp_monthly():
+    session = Session(engine)
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     results = session.query(Measurement.tobs).\
       filter(Measurement.station == 'USC00519281').\
       filter(Measurement.date >= prev_year).all()
     temps = list(np.ravel(results))
+    session.close()
     return jsonify(temps=temps)
 
 #Last route!
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
+    session = Session(engine)
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
     
     if not end:
@@ -83,4 +91,5 @@ def stats(start=None, end=None):
         filter(Measurement.date >= start).\
         filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
+    session.close()
     return jsonify(temps)
